8dd3675f051268bcc9591e702cae2dac77e58b6b
thereayou44 <92093381+thereayou44@users.noreply.github.com>
2023-11-06 05:36:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from src.Gui import GUI
 
 if __name__ == "__main__":
+    data_folder = "data"
     if not os.path.exists(data_folder):
         os.makedirs(data_folder)
     root = tk.Tk()
